f6d5d01c540c571f02867a1ed4566de30d458b30
Michael Ferris <mike.ferris@hotmail.com>
2015-04-23 23:57:50 -0400

    Bug fix

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = {
   modifiers: {
     frontend: {
       react: require("./modifiers/react"),
-      "no-code-style": require("./modifiers/code_style"),
     },
     global: {
       "no-code-style": require("./modifiers/code_style"),
